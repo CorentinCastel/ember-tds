0f400a229da6bf1324fe6a9830523ac5defd3c50
Corentin Castel <corentin.castel0@gmail.com>
2021-01-25 12:08:57 +0100

    fonction update et clear fonctionnelles affichage focntionnel

---
+++ app/controllers/ex1.js
@@ -0,0 +1,40 @@
+import Controller from '@ember/controller';
+import {action} from '@ember/object';
+import {tracked} from '@glimmer/tracking';
+
+
+export default class Ex1Controller extends Controller {
+  @tracked content = "";
+  @tracked info = "";
+  MAX = 100;
+
+  get size(){
+    return this.MAX - this.content.length;
+  }
+
+  get style() {
+    if (this.size >= 50) {
+      return "alert-info";
+    }
+    if (this.size >=20){
+      return "alert-warning";
+    }
+    return "alert-danger";
+  }
+  @action
+  clear(){
+    this.content = "";
+    this.info = "";
+  }
+  @action
+  save(){
+    console.log(this.content);
+    this.content = this.content;
+    this.info = "Contenu sauvegardé";
+  }
+  @action
+  update(){
+    console.log("update");
+    this.info = "Contenu modifié";
+  }
+}
